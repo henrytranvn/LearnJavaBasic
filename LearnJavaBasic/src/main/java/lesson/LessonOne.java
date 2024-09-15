f732eeabe3ebba01d5aacacbd9674007543c1061
henrytranvn <henrytran.home@gmail.com>
2024-09-15 13:08:39 +0700

    lesson two

--- LearnJavaBasic/src/main/java/lesson/LessonOne.java
+++ LearnJavaBasic/src/main/java/lesson/LessonOne.java
@@ -4,13 +4,13 @@ public class LessonOne {
     public static void main(String[] args) {
         int a = 15;
         int b = 3;
-        int resultPlus = a + b;
-        int resultMinus = a - b;
-        int resultMulti = a * b;
-        int resultDivision = a / b;
-        System.out.println(a + " + " + b + " = " + resultPlus);
-        System.out.println(a + " - " + b + " = " + resultMinus);
-        System.out.println(a + " * " + b + " = " + resultMulti);
-        System.out.println(a + " / " + b + " = " + resultDivision);
+        int intResultPlus = a + b;
+        int ResultMinus = a - b;
+        int intResultMulti = a * b;
+        int intResultDivision = a / b;
+        System.out.println(a + " + " + b + " = " + intResultPlus);
+        System.out.println(a + " - " + b + " = " + ResultMinus);
+        System.out.println(a + " * " + b + " = " + intResultMulti);
+        System.out.println(a + " / " + b + " = " + intResultDivision);
     }
 }
---
+++ LearnJavaBasic/src/main/java/lesson/LessonTwo.java
@@ -0,0 +1,27 @@
+package lesson;
+
+public class LessonTwo {
+    public static void main(String[] args) {
+       String strContent = "Automation Testing Alphaway Tutorial Online";
+       int intCountCharacter = strContent.length();
+
+       String strContentOneExpected = "Testing";
+       boolean isExistValue = strContent.contains(strContentOneExpected);
+
+       String strContentTwoExpected = "Automation";
+       boolean isStartValue = strContent.startsWith(strContentTwoExpected);
+
+       String strContentThreeExpected = "Tutorial";
+       int idxValue = strContent.indexOf(strContentThreeExpected);
+
+       String strOldValue = "Online";
+       String strNewValue = "offline";
+       String strResultReplaceValue = strContent.replace(strOldValue, strNewValue);
+
+       System.out.println("So luong ky tu co trong chuoi la: " + intCountCharacter);
+       System.out.println("Kiem tra gia tri " + strContentOneExpected + " co trong chuoi khong? Ket qua: " + isExistValue);
+       System.out.println("Kiem tra gia tri " + isStartValue + " co bat dau trong chuoi khong? Ket qua: " + isStartValue);
+       System.out.println("Vi tri cua gia tri " + strContentThreeExpected + " trong chuoi co vi tri la " + idxValue);
+       System.out.println("Thay the gia tri cu " + strOldValue + " thanh gia tri moi " + strNewValue + " trong chuoi. Ket qua thay the la: " + strResultReplaceValue);
+    }
+}
