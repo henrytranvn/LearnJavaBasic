4e4b6b1af34cdb465b0d49d42027f3bcba25034e
Jun Daniel <junfpt86@gmail.com>
2024-09-15 12:22:28 +0700

    lesson one

---
+++ LearnJavaBasic/src/main/java/lesson/LessonOne.java
@@ -0,0 +1,16 @@
+package lesson;
+
+public class LessonOne {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+        int resultPlus = a + b;
+        int resultMinus = a - b;
+        int resultMulti = a * b;
+        int resultDivision = a / b;
+        System.out.println(a + " + " + b + " = " + resultPlus);
+        System.out.println(a + " - " + b + " = " + resultMinus);
+        System.out.println(a + " * " + b + " = " + resultMulti);
+        System.out.println(a + " / " + b + " = " + resultDivision);
+    }
+}
